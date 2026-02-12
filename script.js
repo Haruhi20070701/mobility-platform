cf563dcbde6e1cf6f755bdbb3295630ec5fabd8f
春燈 <haruhi20070701@icloud.com>
2026-02-12 22:36:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     initSmoothScroll();
     initScrollAnimations();
     initFAQ();
-    initContactForm();
+ // initContactForm();
     initBackToTop();
 });
 
